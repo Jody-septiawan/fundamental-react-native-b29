e2c0765223258aa33965ac5cf059f794e544b963
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-08 17:01:41 +0900

    update

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View } from 'react-native';
 
 //Import Screen
+import State from './src/screens/state';
 
 export default function App() {
   return (
-    <View>
+    <View style={{ marginTop: 40 }}>
       <StatusBar />
-      {/* Use Component */}
+      <State />
     </View>
   );
 }
--- src/screens/state.js
+++ src/screens/state.js
@@ -1,18 +1,33 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function State() {
   // Init State
+  const [counter, setCounter] = useState(10);
 
   //Create Function Add
+  function add() {
+    setCounter(counter + 1);
+  }
 
   //Create Function Less
+  function less() {
+    setCounter(counter - 1);
+  }
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+      <Text>{counter}</Text>
+
+      <TouchableOpacity onPress={add}>
+        <Text>Add</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={less}>
+        <Text>Less</Text>
+      </TouchableOpacity>
     </View>
   );
 }
