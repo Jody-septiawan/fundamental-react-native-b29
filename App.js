79c6d60106a5dce130d9fc38852708025b0921e6
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-08 16:32:01 +0900

    update

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View } from 'react-native';
 
 //Import Screen
+import EmbedExpression from './src/screens/embedExpression';
 
 export default function App() {
   return (
     <View>
-      <StatusBar />
-      {/* Use Component */}
+      <EmbedExpression />
     </View>
   );
 }
