9105a11dbf461e0757d5fc4274bac5d9edc1f17b
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-08 16:41:37 +0900

    update

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View } from 'react-native';
 
 //Import Screen
+import Event from './src/screens/event';
 
 export default function App() {
   return (
-    <View>
+    <View style={{ marginTop: 40 }}>
       <StatusBar />
-      {/* Use Component */}
+      <Event />
     </View>
   );
 }
--- src/screens/event.js
+++ src/screens/event.js
@@ -1,14 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default function Event() {
   // Create Function Here
+  function Greeting() {
+    return alert('Halo, selamat siang semuanya');
+  }
 
   return (
     <View>
-      <StatusBar />
-      {/* Code Here */}
+      <Text>Jika menekan 'Click Here' akan muncul alert</Text>
+      <TouchableOpacity onPress={() => alert('Hallo apa kabar')}>
+        <Text>Click Here</Text>
+      </TouchableOpacity>
+
+      <Text style={{ marginTop: 20 }}>
+        Jika menekan 'Greeting' akan muncul alert
+      </Text>
+      <TouchableOpacity onPress={Greeting}>
+        <Text>Greeting</Text>
+      </TouchableOpacity>
     </View>
   );
 }
