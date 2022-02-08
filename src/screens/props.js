a0e7e9803709ba8873d627205af878e2119459b5
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-08 16:53:03 +0900

    update

--- src/screens/props.js
+++ src/screens/props.js
@@ -1,14 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+import List from '../components/list';
 
 export default function Props() {
   // Create variable to insert link Image
+  let pic = {
+    uri: 'https://elitetraveler.com/wp-content/uploads/2019/07/Screen-Shot-2019-07-05-at-10.31.44-768x513.png',
+  };
 
   return (
     <View>
       <StatusBar />
-      {/* Code Here */}
+
+      <Text>On the image element using the default props, namely source</Text>
+      <Image source={pic} style={{ width: '100%', height: 200 }} />
+
+      <List data="BMW" />
+      <List data="Mercedes-Benz" />
+      <List data="Lamborghini" />
     </View>
   );
 }
